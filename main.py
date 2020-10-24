8db3cf17036d7035882d9c7f52cd6d5476981984
Athul Cyriac Ajay <athul8720@gmail.com>
2020-10-24 23:38:07 +0530

    Image Temp Cache

--- main.py
+++ main.py
@@ -2,54 +2,79 @@ from fastapi import FastAPI
 from typing import Optional
 import requests
 import textwrap
+import os
 from io import BytesIO
-from PIL import Image,ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont
 from bs4 import BeautifulSoup
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse,FileResponse
+import tempfile
 
+temp_dir = tempfile.TemporaryDirectory()
 app = FastAPI()
 
-def GetLinkData(url:str)->str:
-    soup = BeautifulSoup(requests.get(url).content,"html.parser")
-    description = soup.find("meta", property="og:description").get('content')
-    title = soup.find("meta", property="og:title").get('content')
+
+def GetLinkData(url: str) -> str:
+    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+    description = soup.find("meta", property="og:description").get("content")
+    title = soup.find("meta", property="og:title").get("content")
     return title, description
 
-def drawImage(title,description,url):
-    pass
+
+def drawImage(title, description, url):
+    wrapper = textwrap.TextWrapper(width=60)
+    word_list = wrapper.wrap(text=description)
+    caption_new = ""
+    for ii in word_list[:-1]:
+        caption_new = caption_new + ii + "\n"
+    caption_new += word_list[-1]
+    img = Image.open("bg-1.png")
+    draw = ImageDraw.Draw(img)
+    print(len(title))
+    draw.text((39, 69), url, "#e6e7eb", font=ImageFont.truetype("ibm.ttf", 14))
+    if len(title) > 30:
+        draw.text((31, 116), title, "white", font=ImageFont.truetype("ibm.ttf", 26))
+    else:
+        draw.text((31, 116), title, "white", font=ImageFont.truetype("ibm.ttf", 28))
+    draw.text((31, 196), caption_new, "tomato", font=ImageFont.truetype("fira.ttf", 18))
+    rgb_im = img.convert("RGB")
+    rgb_im.save(f"{temp_dir.name}/{url}.jpeg", "JPEG", quality=100, progressive=True)
+    img_io = BytesIO()
+    rgb_im.save(img_io, "JPEG", quality=100, progressive=True)
+    img_io.seek(0)
+    return img_io
+
+
+def checkImageinDir(url):
+    for _, _, f in os.walk(temp_dir.name):
+        for file in f:
+            if url in file:
+                print("Kitti",file)
+                return True
+            else:
+                print("Illa")
+                return False
+
+
 @app.get("/")
 def getHey():
     return {"message": "Hello World"}
 
+
 @app.get("/img")
 async def getUrlData(url: Optional[str] = None):
-    siteData=GetLinkData(url)
-    baseurl = url.strip("https://").split("/")
+    siteData = GetLinkData(url)
     title = siteData[0]
-    description =siteData[1]
-    burl = baseurl[0]
-    wrapper = textwrap.TextWrapper(width=70) 
-    word_list = wrapper.wrap(text=description)
-    caption_new = ''
-    for ii in word_list[:-1]:
-        caption_new = caption_new + ii + '\n'
-    caption_new += word_list[-1]
-    img = Image.open('bg-1.png')
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("ibm.ttf",21)
-
-    w,h = draw.textsize(caption_new, font=font)
-    W,H = img.size
-    print(len(title))
-    draw.text((39,69),burl,"#e6e7eb",font=ImageFont.truetype("ibm.ttf",14))
-    if len(title) > 30:
-        draw.text((31,116),title,"white",font=ImageFont.truetype("ibm.ttf",24))
+    description = siteData[1]
+    try:
+        sufUrl = url.strip("https://").split("/")[1]
+    except IndexError:
+        sufUrl = url.strip("https://")
+    if checkImageinDir(sufUrl) is True:
+        return FileResponse(f"{temp_dir.name}/{sufUrl}.jpeg")
     else:
-        draw.text((31,116),title,"white",font=ImageFont.truetype("ibm.ttf",28))
-    draw.text((31,196),caption_new,"tomato",font=ImageFont.truetype("ibm.ttf",18))
-    rgb_im=img.convert('RGB')
-    img_io = BytesIO()
-    rgb_im.save(img_io, 'JPEG', quality=100, progressive=True)
-    img_io.seek(0)
-    return StreamingResponse(img_io, media_type="image/jpeg",
-    headers={'Content-Disposition': 'inline; filename="Image.jpeg"'})
+        img = drawImage(title, description, sufUrl)
+        return StreamingResponse(
+            img,
+            media_type="image/jpeg",
+            headers={"Content-Disposition": 'inline; filename="Image.jpeg"'},
+        )
